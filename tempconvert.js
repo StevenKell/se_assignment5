cc9cc2ea8e3cbaeb1c0c2f2ab8be659d9d0b741e
StevenKell <steventkell@gmail.com>
2024-07-30 18:05:21 -0400

    Improve frontend.

--- tempconvert.js
+++ tempconvert.js
@@ -2,6 +2,8 @@ let celsiusInput = document.getElementById('inputCelsius').value
 const tempsubmitbtn = document.querySelector('.tempsubmitbtn')
 let fout = document.getElementById('fout')
 
+
+
 /*Arrow to convert C to F*/
 let fahrenheitOutPut = celsiusInput => (celsiusInput * 1.8) + 32;
 let celsiusOutPut = fahrenheitInput => (fahrenheitInput - 32) / 1.8;
@@ -10,5 +12,6 @@ console.log(fahrenheitOutPut(45));
 
 tempsubmitbtn.addEventListener('click', function () {
 
-    fout.textContent = fahrenheitOutPut('celsiusInput');
+    fout.innerHTML = 6;  /*fahrenheitOutPut('celsiusInput');*/
+    console.log("fahrenheitOutPut")
 })
